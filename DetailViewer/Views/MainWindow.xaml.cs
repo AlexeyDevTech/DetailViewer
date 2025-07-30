117d9523b301538164668a88536d369997b03e45
Alexey <cbyat0@mail.ru>
2025-07-30 16:17:11 +0300

    add installer

--- DetailViewer/Views/MainWindow.xaml.cs
+++ DetailViewer/Views/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-using System.ComponentModel;
-using System.Windows;
+using System.ComponentModel;using System.Windows;using System.IO;using System.Reflection;using Newtonsoft.Json;using System.Threading.Tasks;using System;
 
 namespace DetailViewer.Views
 {
@@ -12,6 +11,7 @@ namespace DetailViewer.Views
         {
             InitializeComponent();
             Closing += MainWindow_Closing;
+            Loaded += async (s, e) => await CheckForUpdatesAsync();
         }
 
         private void MainWindow_Closing(object sender, CancelEventArgs e)
@@ -21,5 +21,39 @@ namespace DetailViewer.Views
             // Скрываем окно вместо закрытия
             Hide();
         }
+
+        private async Task CheckForUpdatesAsync()
+        {
+            try
+            {
+                // ЗАМЕНИТЕ ПУТЬ НА ВАШ РЕАЛЬНЫЙ СЕТЕВОЙ ПУТЬ
+                string versionFilePath = "\\192.168.157.29\\cod2\\soft\\version.json";
+                string content = File.ReadAllText(versionFilePath);
+                var remoteVersion = JsonConvert.DeserializeObject<VersionInfo>(content);
+
+                var assembly = Assembly.GetExecutingAssembly();
+                var localVersion = new Version(assembly.GetName().Version.ToString());
+
+                if (new Version(remoteVersion.Version) > localVersion)
+                {
+                    var result = MessageBox.Show($"Доступна новая версия {remoteVersion.Version}. Хотите установить ее?", "Обновление", MessageBoxButton.YesNo, MessageBoxImage.Information);
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(remoteVersion.DownloadUrl) { UseShellExecute = true });
+                        Application.Current.Shutdown();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Ошибки (например, нет доступа к сети) будут проигнорированы
+            }
+        }
+    }
+
+    public class VersionInfo
+    {
+        public string Version { get; set; }
+        public string DownloadUrl { get; set; }
     }
 }
