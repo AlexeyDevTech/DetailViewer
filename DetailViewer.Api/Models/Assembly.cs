187d8b61a313000ac3aa7a93a6e1230119331c3e
alexey <strannik9717@hotmail.com>
2025-08-21 16:11:10 +0300

    api work part 1

--- DetailViewer.Api/Models/Assembly.cs
+++ DetailViewer.Api/Models/Assembly.cs
@@ -6,40 +6,22 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DetailViewer.Api.Models
 {
-    /// <summary>
-    /// Сущность, представляющая сборку.
-    /// </summary>
     public class Assembly
     {
         [Key]
         public int Id { get; set; }
 
-        /// <summary>
-        /// Идентификатор ЕСКД номера.
-        /// </summary>
         public int EskdNumberId { get; set; }
 
-        /// <summary>
-        /// ЕСКД номер сборки.
-        /// </summary>
         public ESKDNumber EskdNumber { get; set; }
 
-        /// <summary>
-        /// Наименование сборки.
-        /// </summary>
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        /// <summary>
-        /// Материал сборки.
-        /// </summary>
-        public string Material { get; set; }
+        public string? Material { get; set; }
 
-        /// <summary>
-        /// Автор сборки.
-        /// </summary>
-        public string Author { get; set; }
+        public string? Author { get; set; }
 
         [Timestamp]
-        public byte[] Version { get; set; }
+        public byte[]? Version { get; set; }
     }
 }
--- DetailViewer.Api/Models/Product.cs
+++ DetailViewer.Api/Models/Product.cs
@@ -5,40 +5,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace DetailViewer.Api.Models
 {
-    /// <summary>
-    /// Сущность, представляющая изделие.
-    /// </summary>
     public class Product
     {
         [Key]
         public int Id { get; set; }
 
-        /// <summary>
-        /// Идентификатор ЕСКД номера.
-        /// </summary>
         public int EskdNumberId { get; set; }
 
-        /// <summary>
-        /// ЕСКД номер изделия.
-        /// </summary>
         public ESKDNumber EskdNumber { get; set; }
 
-        /// <summary>
-        /// Наименование изделия.
-        /// </summary>
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        /// <summary>
-        /// Материал изделия.
-        /// </summary>
-        public string Material { get; set; }
+        public string? Material { get; set; }
 
-        /// <summary>
-        /// Автор изделия.
-        /// </summary>
-        public string Author { get; set; }
+        public string? Author { get; set; }
 
         [Timestamp]
-        public byte[] Version { get; set; }
+        public byte[]? Version { get; set; }
     }
 }
